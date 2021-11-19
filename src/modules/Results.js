a07db3a236c3b3dce3c47d9669d85b801812f02a
peeweemiwie <miwayagi@gmail.com>
2021-11-19 16:34:07 -0500

    Render phonetics

--- src/modules/Results.js
+++ src/modules/Results.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import MeaningArray from './MeaningArray';
+import Phonetics from './Phonetics';
 
 const Results = (props) => {
 	// console.log('props.data', props.data);
 	const data = props.data;
 	const word = data.word;
-	const phoneticsText = data.phonetics[0].text;
-	const phoneticsAudio = data.phonetics[0].audio;
+	// const phoneticsText = data.phonetics[0].text;
+	// const phoneticsAudio = data.phonetics[0].audio;
 
 	return (
 		<div className='Meaning'>
 			<h2>{word}</h2>
-			<div className='phonetic-text'>[{phoneticsText}]</div>
-			<div className='phonetic-audio'>{phoneticsAudio}</div>
+			{data.phonetics && <Phonetics data={data.phonetics} />}
+
+			{/* <div className='phonetic-text'>[{phoneticsText}]</div>
+			<div className='phonetic-audio'>{phoneticsAudio}</div> */}
 			<MeaningArray array={data.meanings} />
 			<pre>{JSON.stringify(props.data, null, 2)}</pre>
 		</div>
